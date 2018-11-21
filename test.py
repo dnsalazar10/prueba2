b1dd427927b6af4abf4cb349c02e2476f07ab928
dnsalazar10 <dn.salazar10@uniandes.edu.co>
2018-11-21 18:19:06 +0000

    jeje

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # Ejercicio5
-# imprima el mensaje: "hola mundo!" 
+# imprima el mensaje: "hola mundo super CRUEL!" 
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-print("hola mundo cruel")
+print("hola mundo SUPER cruel")
